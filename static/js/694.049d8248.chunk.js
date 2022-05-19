fad405ff3339c4b82f8a00717aee9779643838dc
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2022-05-19 10:25:49 +0000

    Deploying to gh-pages from @ rudenko-vl/goit-react-hw-08-phonebook@639c20639dd5058ebb02b6b17c5af8c9b0b7874d 🚀

---
+++ static/js/694.049d8248.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[694],{3897:function(e,t,r){r.r(t);var s=r(521),a=r(3329);t.default=function(){return(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(s.h4,{}),(0,a.jsx)(s.Dx,{title:"Authorization form"}),(0,a.jsx)(s.p$,{}),(0,a.jsx)(s.$_,{})]})}}}]);
+//# sourceMappingURL=694.049d8248.chunk.js.map
